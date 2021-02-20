3da6ea1b8316ad47b4adff484aaa39c2632bea6a
DELL <DELL@DELL-PC>
2021-02-19 23:25:22 -0600

    primer comit

---
+++ src/git/pkg66187/Git66187.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.pkg66187;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Git66187 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre =  " Alondra Palma " ;
+        String matricula =  " 66187 " ;
+        System.out.println(" Soy "  + nombre +  " y mi matricula es: "  + matricula);
+        
+        int numero =  23;
+        int saldo = 3000;
+        int edad = 19;
+    }
+    
+}
